2ec60bf0746f623e1ef8dcdd77d95fffbcf9167d
Esmaeil Bahrani Fard <esmaeil94@gmail.com>
2022-12-06 23:20:45 +0330

    improve environment of pinoox

--- pincore/boot/boot.php
+++ pincore/boot/boot.php
@@ -9,10 +9,11 @@
  * @link https://www.pinoox.com/
  * @license  https://opensource.org/licenses/MIT MIT License
  */
+
 use pinoox\boot\Loader;
 
-define('PINOOX_DEFAULT_LANG', 'en');
-define('PINOOX_PATH',realpath(dirname(__FILE__) . '/../..').DIRECTORY_SEPARATOR);
+const PINOOX_DEFAULT_LANG = 'en';
+define('PINOOX_PATH', realpath(dirname(__FILE__) . '/../..') . DIRECTORY_SEPARATOR);
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . "Loader.php");
 
 Loader::boot();
