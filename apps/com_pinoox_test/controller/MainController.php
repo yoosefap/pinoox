3de14ad771c394f924f26ad67433cf5c5f20450d
Esmaeil <esmaeil94@gmail.com>
2023-02-04 16:10:48 +0330

    add test app

---
+++ apps/com_pinoox_test/controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *      ****  *  *     *  ****  ****  *    *
+ *      *  *  *  * *   *  *  *  *  *   *  *
+ *      ****  *  *  *  *  *  *  *  *    *
+ *      *     *  *   * *  *  *  *  *   *  *
+ *      *     *  *    **  ****  ****  *    *
+ * @author   Pinoox
+ * @link https://www.pinoox.com/
+ * @license  https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace pinoox\app\com_pinoox_test\controller;
+
+
+use pinoox\component\kernel\controller\Controller;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return 'com_pinoox_test <br><h1>Index</h1>';
+    }
+}
---
+++ apps/com_pinoox_test/router/routes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *      ****  *  *     *  ****  ****  *    *
+ *      *  *  *  * *   *  *  *  *  *   *  *
+ *      ****  *  *  *  *  *  *  *  *    *
+ *      *     *  *   * *  *  *  *  *   *  *
+ *      *     *  *    **  ****  ****  *    *
+ * @author   Pinoox
+ * @link https://www.pinoox.com/
+ * @license  https://opensource.org/licenses/MIT MIT License
+ */
+
+use function pinoox\router\{get};
+use pinoox\app\com_pinoox_welcome\controller\MainController;
+
+get(
+    path: '/',
+    action: [MainController::class, 'index'],
+    name: 'index',
+);
