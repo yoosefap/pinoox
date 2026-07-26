535fafb3e64dcf476b9b075175b140111d7a6a1f
Esmaeil Bahrani Fard <44259840+esmaeilbahrani@users.noreply.github.com>
2026-07-26 12:39:24 +0330

    chore: bump platform version to 3.3.11

--- platform/pinoox.config.php
+++ platform/pinoox.config.php
@@ -11,6 +11,6 @@ return [
     | Kernel version: pincore/config/pincore.config.php
     |
     */
-    'version_code' => 51,
-    'version_name' => '3.3.10',
+    'version_code' => 52,
+    'version_name' => '3.3.11',
 ];
