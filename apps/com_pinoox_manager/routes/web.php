7a1ee9c03e065084f95a1e9c1cd5b9cb6bd116cd
yoosefap <yoosefalipour@gmail.com>
2026-07-28 15:01:26 +0330

    fix: allow all HTTP methods on app-view proxy routes

--- apps/com_pinoox_manager/routes/web.php
+++ apps/com_pinoox_manager/routes/web.php
@@ -3,9 +3,10 @@
 use App\com_pinoox_manager\Controller\AppViewController;
 use Pinoox\Portal\View;
 use function Pinoox\Router\get;
+use function Pinoox\Router\{route_match,any};
 
-get('app/{packageName}', [AppViewController::class, 'run'])->name('app.run');
-get('app/{packageName}/{subPath}', [AppViewController::class, 'run'])
+any('app/{packageName}', [AppViewController::class, 'run'])->name('app.run');
+any('app/{packageName}/{subPath}', [AppViewController::class, 'run'])
     ->name('app.run.sub')
     ->defaults(['subPath' => ''])
     ->filters(['subPath' => '.+']);
