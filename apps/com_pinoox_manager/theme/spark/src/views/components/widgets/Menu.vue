9fd93136ad47393cd6e55ffa80010dad36f80852
Esmaeil Bahrani Fard <44259840+esmaeilbahrani@users.noreply.github.com>
2026-08-05 15:35:13 +0330

    fix: resolve manager JWT token_key for app-view preview

--- apps/com_pinoox_manager/theme/spark/src/views/components/widgets/Menu.vue
+++ apps/com_pinoox_manager/theme/spark/src/views/components/widgets/Menu.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="toolbarMenu">
-    <Icon :is="icon" class="toolbarMenu__icon" size="sm"/>
+    <Icon :is="iconComponent" class="toolbarMenu__icon" size="sm"/>
     <span class="toolbarMenu__label">{{ label }}</span>
   </div>
 </template>
 
 <script setup>
+import { computed, markRaw } from 'vue';
 
 const props = defineProps({
   icon: {
-    type: String,
+    type: [String, Object, Function],
     default: null,
   },
   label: {
     type: String,
     required: true,
   },
+});
+
+const iconComponent = computed(() => {
+    if (!props.icon) {
+        return null;
+    }
+
+    if (typeof props.icon === 'string') {
+        return props.icon;
+    }
+
+    if (typeof props.icon === 'function' || typeof props.icon === 'object') {
+        return markRaw(props.icon);
+    }
+
+    return props.icon;
 });
 </script>
